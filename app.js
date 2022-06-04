0bac430d1482f86145f83dc35e7fe6ad9be6e1f6
Ax-cd <axcd.coder@gmail.com>
2022-06-04 14:06:30 +0200

    write functions to change state and display rating

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-document.addEventListener("DOMContenLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const submit = document.getElementById("submit-btn");
+  const ratingCard = document.querySelector("#rating-state");
+  const thankCard = document.querySelector("#thank-state");
+  const ratingBtns = document.querySelectorAll(".rating-btn");
+  const showRating = document.querySelector(".rating");
+
+  function changeState() {
+    ratingCard.classList.add("display");
+    thankCard.classList.remove("display");
+  }
+  submit.addEventListener("click", changeState);
+
+  function displayRating() {
+    ratingBtns.forEach(function (button) {
+      button.onclick = function (e) {
+        let value = this.innerHTML; // get the value of the button clicked
+        showRating.innerHTML = value; // display value selected in span
+      };
+    });
+  }
+  displayRating();
+});
